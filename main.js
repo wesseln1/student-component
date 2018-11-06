f2f274c45fcdba27cb10a97c3cb0bf7e9d5a7ca2
wesseln1 <nwessel429@gmail.com>
2018-11-06 15:15:14 -0600

    student component practice. green for passing students, red for failing

---
+++ main.js
@@ -0,0 +1,110 @@
+
+const students = [
+    {
+        name: "Chris Miller",
+        class: "History",
+        info: "Failed last exam",
+        score: 59
+    },
+    {
+        name: "Courtney Seward",
+        class: "History",
+        info: "Has completed all homework",
+        score: 91
+    },
+    {
+        name: "Garrett Ward",
+        class: "History",
+        info: "Wonderful at helping other students",
+        score: 88
+    },
+    {
+        name: "John Dulaney",
+        class: "History",
+        info: "Has never missed a class or exam",
+        score: 92
+    },
+    {
+        name: "Greg Lawrence",
+        class: "History",
+        info: "Sub-par performance all around",
+        score: 64
+    },
+    {
+        name: "Leah Duvic",
+        class: "History",
+        info: "Wonderful student",
+        score: 97
+    },
+    {
+        name: "Jesse Page",
+        class: "History",
+        info: "Smokes too much. Distracting.",
+        score: 76
+    },
+    {
+        name: "Kevin Haggerty",
+        class: "History",
+        info: "Falls asleep in class",
+        score: 79
+    },
+    {
+        name: "Max Wolf",
+        class: "History",
+        info: "Talks too much",
+        score: 83
+    },
+    {
+        name: "Lissa Goforth",
+        class: "History",
+        info: "Asks pointless, unrelated questions",
+        score: 78
+    },
+    {
+        name: "Tyler Bowman",
+        class: "History",
+        info: "When was the last time he attended class?",
+        score: 48
+    },
+    {
+        name: "Ray Medrano",
+        class: "History",
+        info: "Needs to contribute to in-class discussions",
+        score: 95
+    }
+  ]
+  
+  const container = document.querySelector('#container');
+  
+  const h1 = (title, style) => {
+      return `<h1 class="${style}">${title}</h1>`
+  }
+  
+  const section = (title, style) => {
+      return `<section class="bordered dashed ${style}">${title}</section>`
+  }
+  
+  const aside = (title, style) => {
+      return `<aside class="${style}">${title}</aside>`
+  }
+  
+  for (student of students) {
+      let studentComponent = ""
+      if (student.score >= 60) {
+          studentComponent = `<div class="student">
+          ${h1(student.name, "xx-large passing")}
+          ${section(student.class, "section--padded")}
+          ${aside(student.info, "pushRight")}
+      </div>`
+      } else {
+        studentComponent = `
+        <div class="student">
+            ${h1(student.name, "xx-large failing")}
+            ${section(student.class, "section--padded")}
+            ${aside(student.info, "pushRight")}
+        </div>`
+      }
+      container.innerHTML += studentComponent
+  }
+  
+ 
